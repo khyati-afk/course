81832eb8c0e17069f3c84e84140c996c7b753afc
khyati-afk <9636@sanskritischool.edu.in>
2025-03-21 11:15:05 +0530

    Started modules

---
+++ 02_main.py
@@ -0,0 +1,4 @@
+import module_02 as mod
+
+cm = float(input("Enter cm: "))
+print(mod.cmtoinches(cm))
---
+++ module_02.py
@@ -0,0 +1,8 @@
+def cmtoinches(x):
+    y = x * 0.393701
+    return y
+
+
+def inchestocm(x):
+    y = x / 0.393701
+    return y
